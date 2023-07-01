9c88afe221d03d8a3a9b41a867005ece8b335fc1
LeChaps <romain_chapon@hotmail.com>
2023-07-01 10:26:14 +0200

    version 1-7-23

--- website/dist/scripts.js
+++ website/dist/scripts.js
@@ -856,7 +856,7 @@ $(document).ready(function() {
 	};
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Jul 1, 2023 15:00:00").getTime();
+		var countDownDate = new Date("Jul 1, 2023 09:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
@@ -886,7 +886,7 @@ $(document).ready(function() {
 		// If the count down is finished, write some text 
 		if (distance < 0) {
 		 clearInterval(x);
-		 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
+		 document.getElementById("demo").innerHTML = "C'est déjà fini";
 		}
 		}, 1000);
 
